6771c871d886a89452bb3c38092b933ada5e8f75
Chetan Bhasin <chetan.bhasin@jimdo.com>
2022-07-07 12:53:45 +0200

    chore: wip changes

---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+  <div class="bg-auto bg-opacity-30 backdrop-blur-lg drop-shadow-lg">
+    <h1 class="text-3xl font-bold text-center">Hello world!</h1>
+  </div>
+</div>
